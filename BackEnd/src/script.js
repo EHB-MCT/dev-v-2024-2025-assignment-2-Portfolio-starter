378590667809bc18c7ffa5243199c4794109d67d
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2024-11-23 23:27:02 +0100

    feat: set up route for fetching all Smiskis data from MongoDB database and update readme with sources

--- BackEnd/src/script.js
+++ BackEnd/src/script.js
@@ -1,8 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('dummyButton');
-    button.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
-    
-    console.log('JavaScript is working!');
+const express = require('express');
+const cors = require('cors');
+const {
+    MongoClient,
+} = require('mongodb');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.port || 3000;
+
+const client = new MongoClient("mongodb+srv://barrybbeeebenson:2025Bitch@cluster0.eocdgpz.mongodb.net/?retryWrites=true&w=majority");
+
+app.use(cors());
+app.use(express.json())
+
+/*Get all smiskis*/
+app.get('/api/data', async (req, res) => {
+    try {
+        await client.connect();
+
+        const database = client.db('JustLilGuys');
+        const collection = database.collection('Smiskis');
+
+        const data = await collection.find({}).toArray();
+
+        res.json(data);
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port  http://localhost:${port}`);
 });
--- README.md
+++ README.md
@@ -15,4 +15,8 @@ Just Lil Guys is a site made for Smiski collectors to keep track of their Smiski
 ## Sources 
 
 - https://chatgpt.com/share/66f2acb5-45b0-8012-afe1-c3e4dc03017d
+- https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/connect/#std-label-node-connect-to-mongodb
+- https://mongodb.github.io/node-mongodb-native/6.3/
+
+
 
